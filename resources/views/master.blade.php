3878493fa058a13d5b9c28e1dd18e896659caad7
darevski <darevski1@gmail.com>
2017-04-05 14:58:28 +0200

    new pages add 2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -135,80 +135,41 @@
         </div>
     </div>
 </section>
-<!-- /PRICING -->
-<!-- ABOUT US -->
-<section id="about-us">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="about-us text-center">
-                    <h4>Morem ipsum dolor sit amet.</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aliquid obcaecati deleniti minus quisquam quis magnam reprehenderit quaerat molestias rerum, excepturi autem fugiat corporis quidem a ipsum quos beatae! Ut!</p>
-                    <a href="" class="btn btn-send">View More</a>
-                </div>
-            </div>
-            <div class="col-sm-6 our-office">
-                <div id="office-carousel" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="images/office/04.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="images/office/02.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="images/office/03.jpg" alt="">
-                        </div>
-
-                        <a class="office-carousel-left" href="#office-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                        <a class="office-carousel-right" href="#office-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
-                    </div>
-                </div> <!--/#office-carousel-->
-            </div>
-        </div>
-    </div>
-</section>
-<!-- /ABOUT US -->
-<!-- CONTACT -->
 <section id="contact">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="section-title">
-                    <h1>Контакт</h1>
-                    <h2>Побарајте понуда</h2>
-                </div>
-            </div>
-            <div class="col-sm-4 contact-info">
-                <p class="contact-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptatum dolorum, possimus tenetur pariatur officia, quo commodi autem doloribus vero rerum aspernatur quidem temporibus.</p>
-                <p class="st-address"><i class="fa fa-map-marker"></i> <strong>E71 8th Ave, New York NY 21001, US</strong></p>
-                <p class="st-phone"><i class="fa fa-mobile"></i> <strong>+00 123-456-789</strong></p>
-                <p class="st-email"><i class="fa fa-envelope-o"></i> <strong>email@yourdomain.com</strong></p>
-                <p class="st-website"><i class="fa fa-globe"></i> <strong>www.yourdomain.com</strong></p>
+            <div class="col-md-6">
+                <h2>Контакт инфо</h2>
+                <ul>
+                    <li><a href="tel:1234567">+389 70 219 253</a> </li>
+                    <li><a href="tel:1234567">+389 77 710 007</a> </li>
+                    <li>aluplast@gmail.com </li>
 
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
             </div>
-            <div class="col-sm-7 col-sm-offset-1">
-                <form action="php/send-contact.php" class="contact-form" name="contact-form" method="post">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <input type="text" name="name" required="required" placeholder="Name*">
-                        </div>
-                        <div class="col-sm-6">
-                            <input type="email" name="email" required="required" placeholder="Email*">
-                        </div>
-                        <div class="col-sm-6">
-                            <input type="text" name="subject" placeholder="Subject">
-                        </div>
-                        <div class="col-sm-6">
-                            <input type="text" name="website" placeholder="Website">
-                        </div>
-                        <div class="col-sm-12">
-                            <textarea name="message" required="required" cols="30" rows="7" placeholder="Message*"></textarea>
-                        </div>
-                        <div class="col-sm-12">
-                            <input type="submit" name="submit" value="Send Message" class="btn btn-send">
-                        </div>
+            <div class="col-md-6">
+                <form action="{{ url('contact')}}" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label name="email">Емаил:</label>
+                        <input id="email" name="email" class="form-control input-lg" placeholder="Ваша контакт маил адреса">
                     </div>
+
+                    <div class="form-group">
+                        <label name="subject">Наслов:</label>
+                        <input id="subject" name="subject" class="form-control input-lg" placeholder="Наслов на порака">
+                    </div>
+
+                    <div class="form-group">
+                        <label name="message">Порака:</label>
+                        <textarea id="message" name="message" class="form-control" rows="6" placeholder="Порака..."></textarea>
+                    </div>
+
+                    <input type="submit" value="Испрати порака" class="btn btn-success btn-block" style="margin-top: 40px;">
                 </form>
             </div>
         </div>
